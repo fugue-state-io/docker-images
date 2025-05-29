9a1f77845ce43acb1c62960f79ec0e2ede529a14
Jon Zudell <jonathon.zudell@gmail.com>
2025-05-29 11:23:37 -0500

    don't cat sensitive values

--- postfix/entrypoint.sh
+++ postfix/entrypoint.sh
@@ -2,7 +2,5 @@
     set -e
     echo "Generating main.cf from template..."
     envsubst < /etc/postfix/main.cf.template > /etc/postfix/main.cf
-    echo "Generated main.cf:"
-    cat /etc/postfix/main.cf
     echo "Starting Postfix..."
     exec "$@"
